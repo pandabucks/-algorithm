4f714cf3bbd58e0d1428017cbb5c726803dad6ad
Yu Kimura <plus.quolity@gmail.com>
2018-11-08 21:21:58 +0900

    INSERTION SORT(Ruby Implement)

---
+++ insertion_sort/ruby.rb
@@ -0,0 +1,12 @@
+a = [32, 12, 43, 243, 19, 54, 14, 20]
+
+a.each_with_index do |num, i|
+  next if i == 0
+  key = a[i]
+  j = i - 1
+  while j >= 0 and a[j] > key
+    a[j + 1] = a[j]
+    j -= 1
+  end
+  a[j + 1] = key
+end
