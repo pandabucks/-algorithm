6f4ac15a2cc257a43409058dcf618c71bf591301
Yu Kimura <plus.quolity@gmail.com>
2018-11-08 22:11:07 +0900

    INSERTION  SORT C++ Implement

---
+++ insertion_sort/cpp.cpp
@@ -0,0 +1,38 @@
+// copy by https://www.geeksforgeeks.org/insertion-sort/
+#include <stdio.h>
+#include <math.h>
+
+void insertionSort(int arr[], int n)
+{
+   int i, key, j;
+   for (i = 1; i < n; i++)
+   {
+       key = arr[i];
+       j = i-1;
+       while (j >= 0 && arr[j] > key)
+       {
+           arr[j+1] = arr[j];
+           j = j-1;
+       }
+       arr[j+1] = key;
+   }
+}
+
+void printArray(int arr[], int n)
+{
+   int i;
+   for (i=0; i < n; i++)
+       printf("%d ", arr[i]);
+   printf("\n");
+}
+
+int main()
+{
+    int arr[] = {32, 12, 43, 243, 19, 54, 14, 20};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
