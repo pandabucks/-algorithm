d714cb57e500a8a8e0d353acf1c8ac3ffaf6a5f4
Yu Kimura <plus.quolity@gmail.com>
2019-06-23 22:27:42 +0900

    Create at_corder_template.cpp

---
+++ at_corder_template.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <cstring>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+using ll = long long;
+#define int ll
+#define P pair<ll, ll>
+#define Pvec vector<P>
+
+int main() {
+    return 0;
+}
